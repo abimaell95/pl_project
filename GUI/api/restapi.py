e7bf2c30fd242de751328f682a20e6f3d69aa0d6
nicolsss <nicole.gq@hotmail.com>
2020-11-30 23:37:46 -0500

    GUI controller implement

--- GUI/api/restapi.py
+++ GUI/api/restapi.py
@@ -1,8 +1,11 @@
 from  flask import Flask, jsonify,request
 from lexer import  get_token
 from sintactico import get_production
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/')
 def index():
