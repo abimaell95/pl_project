d5418d99402db232b10dd8cb4668ac79e4dd0076
nicolsss <nicole.gq@hotmail.com>
2020-11-30 19:32:31 -0500

    Adding rest api

---
+++ GUI/api/restapi.py
@@ -0,0 +1,28 @@
+from  flask import Flask, jsonify,request
+from lexer import  get_token
+from sintactico import get_production
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Lexer API 1.0"
+
+
+@app.route('/lex')
+def lex():
+    string = request.args.get('s')
+    return jsonify({'data':get_token(string)})
+
+@app.route('/parse')
+def parse():
+    string = request.args.get('s')
+    res = get_production(string)
+    if(len(res)==0):
+        return jsonify("Valid Syntax")
+    else:
+        return jsonify("Invalid Syntax")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
